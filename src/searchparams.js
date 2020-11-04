15302bf884643ffe01ce9892e3b546b7f9b80784
Sulaiman <sulaimain@kobo360.com>
2020-11-04 18:01:18 +0100

    asyn calls to API and update Pet component

--- src/searchparams.js
+++ src/searchparams.js
@@ -1,12 +1,23 @@
 import React, { useEffect, useState } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./hooks/useDropdown";
+import Results from "./results";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seatle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
   const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal
+    })
+    setPets(animals || [])
+  }
 
   useEffect(() => {
     setBreeds([]);
@@ -15,11 +26,14 @@ const SearchParams = () => {
       const breedStrings = breeds.map(({ name }) => name);
       setBreeds(breedStrings);
     }, console.error)
-  }, [animal]);
+  }, [animal, setBreed, setBreeds]);
 
   return (
     <div className="search-params">
-      <form>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        requestPets();
+      }}>
         <label htmlFor="location">
           Location
           <input
@@ -33,6 +47,7 @@ const SearchParams = () => {
         <BreedDropdown />
         <button>Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };
