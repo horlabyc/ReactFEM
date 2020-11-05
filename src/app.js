9b80406d4f1c12b52ac3c5df0e48738f65ddbfbe
Sulaiman <sulaimain@kobo360.com>
2020-11-05 13:03:50 +0100

    Added modal and portal

--- src/app.js
+++ src/app.js
@@ -1,23 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
 import { Link, Router } from '@reach/router';
+import ThemeContext from './context/themeContext';
 import SearchParams from "./searchparams";
 import Details from './details';
 
 const App = () => {
+  const themeHook = useState('peru');
+
   return (
     <React.StrictMode>
-      <div>
-        <header>
-          <Link to="/">
-            Adopt me!
+      <ThemeContext.Provider value={themeHook}>
+        <div>
+          <header>
+            <Link to="/">
+              Adopt me!
           </Link>
-        </header>
-        <Router>
-          <SearchParams path="/" />
-          <Details path="/details/:id" />
-        </Router>
-      </div>
+          </header>
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </div>
+      </ThemeContext.Provider>
     </React.StrictMode>
   );
 };
--- src/details.js
+++ src/details.js
@@ -2,12 +2,16 @@ import React from 'react'
 import pet from '@frontendmasters/pet';
 import Carousel from './carousel';
 import ErrorBoundary from './errorboundary';
+import ThemeContext from './context/themeContext';
+import { navigate } from '@reach/router';
+import Modal from './modals/modal';
 
 class Details extends React.Component {
-  state = { loading: true };
+  state = { loading: true, showModal: false };
   componentDidMount() {
     pet.animal(this.props.id).then(({ animal }) => {
       this.setState({
+        url: animal.url,
         name: animal.name,
         animal: animal.type,
         location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
@@ -18,8 +22,17 @@ class Details extends React.Component {
       })
     }, console.error)
   };
+
+  toggleModal = () => {
+    this.setState({
+      showModal: !this.state.showModal
+    })
+  }
+
+  adoptPet = () => navigate(this.state.url);
+
   render() {
-    const { name, animal, location, description, breed, loading, media } = this.state
+    const { name, animal, location, description, breed, loading, media, showModal } = this.state
     if (loading) {
       return <h1>...loading</h1>
     }
@@ -28,8 +41,25 @@ class Details extends React.Component {
         <Carousel media={media} />
         <h1>{name}</h1>
         <h2>{`${animal} - ${breed} - ${location}`}</h2>
-        <button>Adopt {name}</button>
+        <ThemeContext.Consumer>
+          {([theme]) => (
+            <button style={{ backgroundColor: theme }} onClick={this.toggleModal}>Adopt {name}</button>
+          )}
+        </ThemeContext.Consumer>
         <p>{description}</p>
+        {
+          showModal ? (
+            <Modal>
+              <div>
+                <h1>Would you like to adopt {name}?</h1>
+                <div className="buttons">
+                  <button onClick={this.adoptPet}>Yes</button>
+                  <button onClick={this.toggleModal}>No I am a monster</button>
+                </div>
+              </div>
+            </Modal>
+          ) : null
+        }
       </div>
     );
   }
